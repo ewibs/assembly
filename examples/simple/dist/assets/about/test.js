8606e1410858dfd217e17d2119423f0391112a85
Simon Rothert <puresamari@gmail.com>
2022-06-12 16:13:14 +0200

    removed dist

--- examples/simple/dist/assets/about/test.js
+++
@@ -1,6 +0,0 @@
-window.spbp.append({
-  "@rothert/spbp/modules/page/about": ({ require }, exports) => {
-    require("@rothert/spbp/modules/custom/test-button");
-  },
-});
-window.spbp.require("@rothert/spbp/modules/page/about");
--- examples/simple/dist/assets/index.js
+++
@@ -1,6 +0,0 @@
-window.spbp.append({
-  "@rothert/spbp/modules/page/index": ({ require }, exports) => {
-    require("@rothert/spbp/modules/custom/test-button");
-  },
-});
-window.spbp.require("@rothert/spbp/modules/page/index");
